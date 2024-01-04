4342f2c2f5abc2b2e2628f63389810244a54fd0c
Santosh Bhandari <63558580+santoshvandari@users.noreply.github.com>
2024-01-04 07:14:32 +0545

    Initial

--- SocialAuthentication/settings.py
+++ SocialAuthentication/settings.py
@@ -37,6 +37,8 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
+    # Add the following apps:
+    'Auth'
 ]
 
 MIDDLEWARE = [
