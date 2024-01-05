3326b3da427839923e85462412d185df63b9da8d
Santosh Bhandari <63558580+santoshvandari@users.noreply.github.com>
2024-01-05 17:21:42 +0545

    Initial

--- Auth/views.py
+++ Auth/views.py
@@ -1,7 +1,10 @@
 from django.shortcuts import render
+from django.contrib.auth.decorators import login_required
 
 # Create your views here.
 def log_in(request):
     return render(request,'login.html')
 
-def 
+@login_required(login_url='/login/')
+def home(request):
+    return render(request,'home.html')
