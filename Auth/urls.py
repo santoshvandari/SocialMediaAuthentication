e9d4900ab61bfc35020446b62f2dc10edc21c029
Santosh Bhandari <63558580+santoshvandari@users.noreply.github.com>
2024-01-05 17:36:19 +0545

    Initial

--- Auth/urls.py
+++ Auth/urls.py
@@ -1,8 +1,9 @@
-from django.urls import path
+from django.urls import path,include
 from Auth import views
 
 urlpatterns = [
     path('login/',views.log_in,name='login'),
     path('',views.home,name='home'),
     path('home/',views.home,name='home'),
+    (r'^auth/', include('rest_framework_social_oauth2.urls')),
 ]
