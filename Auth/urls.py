b4cbc614c9f5e3a378672189ab08b462e87515f3
Santosh Bhandari <63558580+santoshvandari@users.noreply.github.com>
2024-01-05 17:41:54 +0545

    Initial

--- Auth/urls.py
+++ Auth/urls.py
@@ -5,5 +5,4 @@ urlpatterns = [
     path('login/',views.log_in,name='login'),
     path('',views.home,name='home'),
     path('home/',views.home,name='home'),
-    (r'^auth/', include('rest_framework_social_oauth2.urls')),
 ]
