83e9fe5f1dd914e67321e349bb81ba09e609e89f
Santosh Bhandari <63558580+santoshvandari@users.noreply.github.com>
2024-01-05 17:37:01 +0545

    Initial

--- SocialAuthentication/settings.py
+++ SocialAuthentication/settings.py
@@ -68,6 +68,9 @@ TEMPLATES = [
                 'django.template.context_processors.request',
                 'django.contrib.auth.context_processors.auth',
                 'django.contrib.messages.context_processors.messages',
+                # adding new preprocessor for social auth 
+                'social_django.context_processors.backends',
+                'social_django.context_processors.login_redirect',
             ],
         },
     },
