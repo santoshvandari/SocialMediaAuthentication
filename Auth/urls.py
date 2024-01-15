4b494e703e1804f0ef3a90593ba6df7a3dea7a92
Santosh Bhandari <63558580+santoshvandari@users.noreply.github.com>
2024-01-15 20:30:49 +0545

    Initial

--- Auth/urls.py
+++
@@ -1,8 +0,0 @@
-from django.urls import path,include
-from Auth import views
-
-urlpatterns = [
-    path('login/',views.log_in,name='login'),
-    path('',views.home,name='home'),
-    path('home/',views.home,name='home'),
-]
