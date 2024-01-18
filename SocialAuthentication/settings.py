0cb08d36594f047378d47b28ad8a215984519aa8
Santosh Bhandari <63558580+santoshvandari@users.noreply.github.com>
2024-01-18 10:34:49 +0545

    Initial

--- SocialAuthentication/settings.py
+++ SocialAuthentication/settings.py
@@ -63,6 +63,9 @@ TEMPLATES = [
                 'django.template.context_processors.request',
                 'django.contrib.auth.context_processors.auth',
                 'django.contrib.messages.context_processors.messages',
+
+                # `allauth` needs this from django
+                'django.template.context_processors.request',
             ],
         },
     },
