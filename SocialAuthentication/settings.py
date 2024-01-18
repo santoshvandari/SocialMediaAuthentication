82b4edf696b6c82cbbbbc692412ce6ce8dc49c55
Santosh Bhandari <63558580+santoshvandari@users.noreply.github.com>
2024-01-18 10:42:17 +0545

    Initial

--- SocialAuthentication/settings.py
+++ SocialAuthentication/settings.py
@@ -51,6 +51,8 @@ MIDDLEWARE = [
     'django.contrib.auth.middleware.AuthenticationMiddleware',
     'django.contrib.messages.middleware.MessageMiddleware',
     'django.middleware.clickjacking.XFrameOptionsMiddleware',
+    # Add the account middleware:
+    "allauth.account.middleware.AccountMiddleware",
 ]
 
 ROOT_URLCONF = 'SocialAuthentication.urls'
